c136d7140936e7d14d0e0dd75f8ffc1a2bb79baa
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-12 21:10:26 +0800

    changes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = "";
+    if (string.length == 0)
+    {
+        return reverse;
+    }
+    for (let i = string.length - 1; i >= 0; i--)
+    {
+        reverse += string[i];
+    }
+    return reverse;
 
 };
 
