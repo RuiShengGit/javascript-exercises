56521db66e0fbdf94de6c900391b7b994ad8b6db
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-14 23:11:37 +0800

    Changes to palindromes and fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let n1 = 0; 
+    let n2 = 1;
+    var nextTerm;
+    if (number < 0)
+    {
+        return "OOPS";
+    }
+    for (let i = 1; i <= number; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1;
 };
 
 // Do not edit below this line
